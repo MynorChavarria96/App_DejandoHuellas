e8c5ecc50a354fecd36391066cb51a55bc80b800
Mynor Chavarria <mychava96@gmail.com>
2024-09-24 23:35:21 -0600

    Se acomodó navbar y se agregó boton dropdown

--- public/js/alertas.js
+++ public/js/alertas.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const logoutLink = document.getElementById('logout-link');
-    if (logoutLink) {
+  const logoutLinks = document.querySelectorAll('.logout'); // Selecciona todos los elementos con la clase 'logout'
+  
+  logoutLinks.forEach(function(logoutLink) {
       logoutLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        Swal.fire({
-          title: 'Confirmar cierre de sesión',
-          text: "¿Seguro que quieres cerrar sesión?",
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Sí, cerrar sesión'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.href = '/users/logout';
-          }
-        });
+          event.preventDefault();  // Evita el comportamiento predeterminado
+          Swal.fire({
+              title: 'Confirmar cierre de sesión',
+              text: "¿Seguro que quieres cerrar sesión?",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Sí, cerrar sesión'
+          }).then((result) => {
+              if (result.isConfirmed) {
+                  window.location.href = '/users/logout';  // Redirige al usuario
+              }
+          });
       });
-    }
   });
+});
